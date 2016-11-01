b1accaf575d94a9efc285fd7df3cf14e146c9a51
Jota Junior <jotavrj@gmail.com>
2016-10-31 23:54:12 -0200

    First endpoints

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, abort
+import json
+from questions import *
+import random
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'it\'s alive'
+
+@app.route('/question/<int:_id>')
+def question(_id):
+    for q in questions:
+        if q['id'] == _id:
+            return json.dumps(q)
+
+    abort(404)
+
+@app.route('/question/random')
+def question_random():
+    return jsonify(random.choice(questions))
+
+@app.route('/question/answer/<int:_id>')
+def answer(_id):
+    ans = request.args.get('choice', None)
+    for q in questions:
+        if q['id'] == _id:
+            return q['correct'] == ans
+
+    return False
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ questions.py
@@ -0,0 +1,21 @@
+questions = [
+        {
+            'id': 1,
+            'question': 'is this love',
+            'A': 'that im feeling',
+            'B': 'is this the love',
+            'C': 'that Im looking for',
+            'correct': 'A',
+            'explanation': 'luv is just like this babe',
+        },
+        {
+            'id': 2,
+            'question': 'never gonna give you up',
+            'A': 'never gonna let you down',
+            'B': 'never gonna turn around and',
+            'C': 'desert you',
+            'correct': 'C',
+            'explanation': 'trust me bae',
+        },
+        ]
+
