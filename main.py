24862d830ea5b7956ba279e3c3558529bb4c82d0
Jota Junior <jotavrj@gmail.com>
2016-11-20 19:41:11 -0200

    Fix character encoding

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from flask import Flask, request, abort
 import json
 from questions import *
@@ -15,13 +16,13 @@ def index():
 def question(_id):
     for q in questions:
         if q['id'] == _id:
-            return json.dumps(q)
+            return json.dumps(q, ensure_ascii=False)
 
     abort(404)
 
 @app.route('/question/random')
 def question_random():
-    return json.dumps(random.choice(questions))
+    return json.dumps(random.choice(questions), ensure_ascii=False)
 
 @app.route('/question/answer/<int:_id>')
 def answer(_id):
